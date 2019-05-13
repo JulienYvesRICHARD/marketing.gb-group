605038381c27d7cdc035d6223032984cc02cdeec
richard <j.richard@gb-group.co>
2019-05-13 01:13:29 -0700

    upadting of the translation and adding the title and sub item via i18n in the new component

--- src/components/Layout/Range_name.vue
+++ src/components/Layout/Range_name.vue
@@ -2,14 +2,14 @@
 <template>
   <div class="container" id="range">
     <a class="anchor" name="range"></a>
-    <h3>Range Spelling</h3>
+    <h3>{{ $t('range_spelling_title')}}</h3>
     <div class="row" >
         <div class="col-12 col-sm-6 col_md-6" v-for="item in range" :key="item">
           <h4 class="text-center mt-2">{{ item.name }}</h4>
           <div class="m-1 card">
             <div class="row p-2 ">
               <div class="col-12 col-sm-12 col-lg-4 range p-3" v-for="machine in item.machine" :key="machine">
-                <h5 class="text-center" v-if="machine.title">Titre et espace</h5>
+                <h5 class="text-center" v-if="machine.title">{{ $t('range_spelling_space')}}</h5>
                 <div v-for="title in machine.title" :key="title">
                   <h5 v-if="title.goodspelling">
                       <img src="/static/img/correct.png">
@@ -20,7 +20,7 @@
                       <span class="wrong rangeSpace">{{ title.wrongspelling }}</span><br>
                   </span>
                 </div>
-                <h5 class="text-center" v-if="machine.stickers">Stickers</h5>
+                <h5 class="text-center" v-if="machine.stickers">{{ $t('range_spelling_sticker')}}</h5>
                 <div v-for="sticker in machine.stickers" :key="sticker">
                   <h5 v-if="sticker.goodspelling">
                       <img src="/static/img/correct.png">
@@ -31,7 +31,7 @@
                       <span class="wrong rangeSpace">{{ sticker.wrongspelling }}</span><br>
                   </span>
                 </div>
-                <h5 class="text-center" v-if="machine.texts">Corps de texte</h5>
+                <h5 class="text-center" v-if="machine.texts">{{ $t('range_spelling_text')}}</h5>
                 <div v-for="text in machine.texts" :key="text">
                   <h5 v-if="text.goodspelling">
                       <img src="/static/img/correct.png">

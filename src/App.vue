9b85c294761ceaf2ca16b38edf7ae4bb44620324
richard <j.richard@gb-group.co>
2019-09-10 15:58:01 +0200

    adding translation and the corresponding key for the top menu

--- src/App.vue
+++ src/App.vue
@@ -16,17 +16,17 @@
                           <img src="" width="25"> GB Group
                       </a>
                       <div class="dropdown-menu dropdown-menu-right" aria-labelledby="gbgroup">
-                          <a class="dropdown-item" href="/#logo">Logos</a>
-                          <a class="dropdown-item" href="/#branding">Branding</a>
-                          <a class="dropdown-item" href="/#audio">Audio</a>
-                          <a class="dropdown-item" href="/#introOutro">Video Intro/Outro</a>
-                          <a class="dropdown-item" href="/#link">Links</a>
-                          <a class="dropdown-item" href="/#subsidiarie">Subsidiaries</a>
-                          <a class="dropdown-item" href="/#press">Press Releases</a>
-                          <a class="dropdown-item" href="/#photo">Photos</a>
-                          <a class="dropdown-item" href="/#print">Print Graphic Charter</a>
-                          <a class="dropdown-item" href="/#powerpoint">Powerpoint Template</a>
-                          <a class="dropdown-item" href="/#icon">Icons</a>
+                          <a class="dropdown-item" href="/#logo">{{ $t('menu_logos') }}</a>
+                          <a class="dropdown-item" href="/#branding">{{ $t('menu_branding') }}</a>
+                          <a class="dropdown-item" href="/#audio">{{ $t('menu_audio') }}</a>
+                          <a class="dropdown-item" href="/#introOutro">{{ $t('menu_video_intro') }}</a>
+                          <a class="dropdown-item" href="/#link">{{ $t('menu_links') }}</a>
+                          <a class="dropdown-item" href="/#subsidiarie">{{ $t('menu_subsidiaries') }}</a>
+                          <a class="dropdown-item" href="/#press">{{ $t('menu_press') }}</a>
+                          <a class="dropdown-item" href="/#photo">{{ $t('menu_photos') }}</a>
+                          <a class="dropdown-item" href="/#print">{{ $t('menu_videos') }}</a>
+                          <a class="dropdown-item" href="/#powerpoint">{{ $t('menu_print') }}</a>
+                          <a class="dropdown-item" href="/#icon">{{ $t('menu_icons') }}</a>
                           <router-link to="/"></router-link>
                       </div>
                   </li>
@@ -35,17 +35,17 @@
                           <img src="" width="25"> Grégoire Besson
                       </a>
                       <div class="dropdown-menu dropdown-menu-right" aria-labelledby="gregoirebesson">
-                          <a class="dropdown-item" href="/gregoire-besson#logo">Logos</a>
-                          <a class="dropdown-item" href="/gregoire-besson#branding">Branding</a>
-                          <a class="dropdown-item" href="/gregoire-besson#range">Range</a>
-                          <a class="dropdown-item" href="/gregoire-besson#introOutro">Video Intro/Outro</a>
-                          <a class="dropdown-item" href="/gregoire-besson#link">Links</a>
-                          <a class="dropdown-item" href="/gregoire-besson#photo">Photos</a>
-                          <a class="dropdown-item" href="/gregoire-besson#newleaflet">New Leaflets</a>
-                          <a class="dropdown-item" href="/gregoire-besson#oldleaflet">Old Leaflets</a>
-                          <a class="dropdown-item" href="/gregoire-besson#video">Videos</a>
-                          <a class="dropdown-item" href="/gregoire-besson#price">Pricelists</a>
-                          <a class="dropdown-item" href="/gregoire-besson#poster">Posters</a>
+                          <a class="dropdown-item" href="/gregoire-besson#logo">{{ $t('menu_logos') }}</a>
+                          <a class="dropdown-item" href="/gregoire-besson#branding">{{ $t('menu_branding') }}</a>
+                          <a class="dropdown-item" href="/gregoire-besson#range">{{ $t('menu_range') }}</a>
+                          <a class="dropdown-item" href="/gregoire-besson#introOutro">{{ $t('menu_video_intro') }}</a>
+                          <a class="dropdown-item" href="/gregoire-besson#link">{{ $t('menu_links') }}</a>
+                          <a class="dropdown-item" href="/gregoire-besson#photo">{{ $t('menu_photos') }}</a>
+                          <a class="dropdown-item" href="/gregoire-besson#newleaflet">{{ $t('menu_new_leaflets') }}</a>
+                          <a class="dropdown-item" href="/gregoire-besson#oldleaflet">{{ $t('menu_old_leaflets') }}</a>
+                          <a class="dropdown-item" href="/gregoire-besson#video">{{ $t('menu_videos') }}</a>
+                          <a class="dropdown-item" href="/gregoire-besson#price">{{ $t('menu_pricelist') }}</a>
+                          <a class="dropdown-item" href="/gregoire-besson#poster">{{ $t('menu_posters') }}</a>
                           <router-link to="/gregoire-besson"></router-link>
                       </div>
                   </li>
@@ -54,14 +54,14 @@
                           <img src="" width="25"> Rabe
                       </a>
                       <div class="dropdown-menu dropdown-menu-right" aria-labelledby="rabe">
-                          <a class="dropdown-item" href="/rabe#ogo">Logos</a>
-                          <a class="dropdown-item" href="/rabe#branding">Branding</a>
-                          <a class="dropdown-item" href="/rabe#introOutro">Video Intro/Outro</a>
-                          <a class="dropdown-item" href="/rabe#link">Links</a>
-                          <a class="dropdown-item" href="/rabe#photo">Photos</a>
-                          <a class="dropdown-item" href="/rabe#leaflet">Leaflets</a>
-                          <a class="dropdown-item" href="/rabe#video">Videos</a>
-                          <a class="dropdown-item" href="/rabe#price">Pricelists</a>
+                          <a class="dropdown-item" href="/rabe#ogo">{{ $t('menu_logos') }}</a>
+                          <a class="dropdown-item" href="/rabe#branding">{{ $t('menu_branding') }}</a>
+                          <a class="dropdown-item" href="/rabe#introOutro">{{ $t('menu_video_intro') }}</a>
+                          <a class="dropdown-item" href="/rabe#link">{{ $t('menu_links') }}</a>
+                          <a class="dropdown-item" href="/rabe#photo">{{ $t('menu_photos') }}</a>
+                          <a class="dropdown-item" href="/rabe#leaflet">{{ $t('menu_leaflets') }}</a>
+                          <a class="dropdown-item" href="/rabe#video">{{ $t('menu_videos') }}</a>
+                          <a class="dropdown-item" href="/rabe#price">{{ $t('menu_pricelist') }}</a>
                           <router-link to="/rabe"></router-link>
                       </div>
                   </li>
@@ -70,10 +70,10 @@
                           <img src="" width="25"> Agriway
                       </a>
                       <div class="dropdown-menu dropdown-menu-right" aria-labelledby="agriway">
-                          <a class="dropdown-item" href="/agriway#logo">Logos</a>
-                          <a class="dropdown-item" href="/agriway#branding">Branding</a>
-                          <a class="dropdown-item" href="/agriway#video">Video Intro/Outro</a>
-                          <a class="dropdown-item" href="/agriway#link">Links</a>
+                          <a class="dropdown-item" href="/agriway#logo">{{ $t('menu_logos') }}</a>
+                          <a class="dropdown-item" href="/agriway#branding">{{ $t('menu_branding') }}</a>
+                          <a class="dropdown-item" href="/agriway#video">{{ $t('menu_video_intro') }}</a>
+                          <a class="dropdown-item" href="/agriway#link">{{ $t('menu_links') }}</a>
                           <router-link to="/agriway"></router-link>
                       </div>
                   </li>

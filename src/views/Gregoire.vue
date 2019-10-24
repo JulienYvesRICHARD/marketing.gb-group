719af8175d1ac79a063c6d75f591316575060682
richard <j.richard@gb-group.co>
2019-10-24 17:53:21 +0200

    updating of the ploughing leaflet in 60 70 index

--- src/views/Gregoire.vue
+++ src/views/Gregoire.vue
@@ -635,11 +635,11 @@ export default {
               },
               {
                 "name": "Index 40 / 50 [EN]",
-                "link": "https://marketing.cellar.services.clever-cloud.com/leaflets/gregoire-besson/ploughing_index_40_50_EN_191024.pdf"
+                "link": "https://marketing.cellar.services.clever-cloud.com/leaflets/gregoire-besson/ploughing_index_40_50_EN_090919.pdf"
               },
               {
                 "name": "Index 60 / 70 [EN]",
-                "link": "https://marketing.cellar.services.clever-cloud.com/leaflets/gregoire-besson/ploughing_index_60_70_EN_090919.pdf"
+                "link": "https://marketing.cellar.services.clever-cloud.com/leaflets/gregoire-besson/ploughing_index_60_70_EN_191024.pdf"
               },
               {
                 "name": "Index 80 / 90 [EN]",

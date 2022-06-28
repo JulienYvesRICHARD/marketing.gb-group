da50cf459b2cea93edc418fa61d31894967c6e3f
Gb Group - Grégoire Besson / Agriway <44161762+JulienYvesRICHARD@users.noreply.github.com>
2022-06-28 17:02:27 +0200

    Update price list

--- src/views/Gregoire.vue
+++ src/views/Gregoire.vue
@@ -1699,7 +1699,7 @@ export default {
         {
           title: 'Machines (IN) €',
           link:
-            'http://marketing.cellar-c2.services.clever-cloud.com/pricelists/gregoire-besson/2022-05-11-pricelist-gregoirebesson-in.pdf',
+            'http://marketing.cellar-c2.services.clever-cloud.com/pricelists/gregoire-besson/2022-06-28-pricelist-gregoirebesson-in.pdf',
           image: {
             src: '/static/img/pricelists/gb-pricelist-in-img.jpg',
             label: 'Price list Gregoire Besson 2022 in euros, english version'
@@ -1708,7 +1708,7 @@ export default {
         {
           title: 'Machines (FR) €',
           link:
-            'http://marketing.cellar-c2.services.clever-cloud.com/pricelists/gregoire-besson/2022-05-11-pricelist-gregoirebesson-fr.pdf',
+            'http://marketing.cellar-c2.services.clever-cloud.com/pricelists/gregoire-besson/2022-06-28-pricelist-gregoirebesson-fr.pdf',
           image: {
             src: '/static/img/pricelists/gb-pricelist-fr-img.jpg',
             label: 'Liste de prix Gregoire Besson 2022 en euros, version française'
@@ -1717,7 +1717,7 @@ export default {
         {
           title: 'Machines (DE) €',
           link:
-            'http://marketing.cellar-c2.services.clever-cloud.com/pricelists/gregoire-besson/2022-05-11-pricelist-gregoirebesson-de.pdf',
+            'http://marketing.cellar-c2.services.clever-cloud.com/pricelists/gregoire-besson/2022-06-28-pricelist-gregoirebesson-de.pdf',
           image: {
             src: '/static/img/pricelists/gb-pricelist-de-img.jpg',
             label: 'Preisliste Gregoire Besson 2022 in Euro, deutsche Fassung'

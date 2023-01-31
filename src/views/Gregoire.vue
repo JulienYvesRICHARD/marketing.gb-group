22cb2d52727e430e6e48022614afabdabf195819
Gb Group - Grégoire Besson / Agriway <44161762+JulienYvesRICHARD@users.noreply.github.com>
2023-01-31 08:39:23 +0100

    Update pricelist Fr

--- src/views/Gregoire.vue
+++ src/views/Gregoire.vue
@@ -1708,7 +1708,7 @@ export default {
         {
           title: 'Machines (FR) €',
           link:
-            'http://marketing.cellar-c2.services.clever-cloud.com/pricelists/gregoire-besson/2023-01-03-pricelist-gregoirebesson-fr.pdf',
+            'http://marketing.cellar-c2.services.clever-cloud.com/pricelists/gregoire-besson/2023-01-31-pricelist-gregoirebesson-fr.pdf',
           image: {
             src: '/static/img/pricelists/gb-pricelist-230-fr-img.jpg',
             label: 'Liste de prix Gregoire Besson 2022 en euros, version française'

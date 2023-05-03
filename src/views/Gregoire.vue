9c2c5b1e690b715d17c8a881f34a9c0ea5a6afa2
Gb Group - Grégoire Besson / Agriway <44161762+JulienYvesRICHARD@users.noreply.github.com>
2023-05-03 17:28:54 +0200

    Add Italian price list

--- src/views/Gregoire.vue
+++ src/views/Gregoire.vue
@@ -1710,6 +1710,15 @@ export default {
           }
         },
         {
+          title: 'Machines (IT) €',
+          link:
+            'http://marketing.cellar-c2.services.clever-cloud.com/pricelists/gregoire-besson/2023-05-03-pricelist-parts-gregoirebesson-it.pdf',
+          image: {
+            src: '/static/img/pricelists/gb-pricelist-231-it-img.jpg',
+            label: 'Listino prezzi Gregoire Besson 2022 in euro, versione italiana'
+          }
+        },
+        {
           title: 'Machines (DE) €',
           link:
             'http://marketing.cellar-c2.services.clever-cloud.com/pricelists/gregoire-besson/2022-12-21-pricelist-gregoirebesson-de.pdf',
